50f221c37e82886ec64f02a01be6951d3e294cb9
Per Grönberg <A2010438@nbsemp.no>
2023-03-01 16:00:41 +0100

    Added normal and rerollable

---
+++ DiceRoller/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DiceRoller.Roller;
+
+Console.WriteLine("Hello, World!");
+
+var iterations = 1000;
+var printTempl = "Roller={0},Target={1}, Rate={2}";
+var rollers = new List<IRoller>();
+rollers.Add(new NormalChargeRoller());
+rollers.Add(new RerollableChargeRoller());
+
+foreach (var roller in rollers)
+{
+    foreach (var target in Enumerable.Range(2, 11))
+    {
+
+        int i = iterations;
+        var success = 0;
+        while (i-- > 0)
+        {
+            if (roller.Roll(target))
+            {
+                success++;
+            }
+        }
+
+        var rate = (success / 1.0) / (iterations / 1.0);
+
+        Console.Out.WriteLine(string.Format(printTempl, roller.GetType().Name, target, rate));
+    }
+}
---
+++ DiceRoller/Roller/IRoller.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller.Roller
+{
+    internal interface IRoller
+    {
+        bool Roll(int target);
+    }
+
+    abstract class RollerBase : IRoller
+    {
+        protected Random Rand { get; }
+
+        protected RollerBase()
+        {
+            Rand = new Random((int)(DateTime.Now.Ticks));
+        }
+
+        public abstract bool Roll(int target);
+    }
+
+    class NormalChargeRoller : RollerBase
+    {
+
+        public override bool Roll(int target)
+        {
+            var a = Rand.Next(6)+1;
+            var b = Rand.Next(6)+1;
+            return (a + b) >= target;
+        }
+    }
+
+    class RerollableChargeRoller : RollerBase
+    {
+
+        public override bool Roll(int target)
+        {
+            var a = Rand.Next(6) + 1;
+            var b = Rand.Next(6) + 1;
+            if ((a + b) < target)
+            {
+                a = Rand.Next(6) + 1;
+                b = Rand.Next(6) + 1;
+            }
+
+            return (a + b) >= target;
+        }
+    }
+}
