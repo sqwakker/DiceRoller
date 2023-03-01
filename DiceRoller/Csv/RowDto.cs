20dc9249507cb4018f97c2079cf6b2918ce8c9f7
Per Grönberg <A2010438@nbsemp.no>
2023-03-01 16:43:46 +0100

    Added normal and rerollable

---
+++ DiceRoller/Csv/RowDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller.Csv
+{
+    public class RowDto
+    {
+        public string? RollerName { get; set; }
+        public  double Target2Rate { get; set; }
+        public double Target3Rate { get; set; }
+        public double Target4Rate { get; set; }
+        public double Target5Rate { get; set; }
+        public double Target6Rate { get; set; }
+        public double Target7Rate { get; set; }
+        public double Target8Rate { get; set; }
+        public double Target9Rate { get; set; }
+        public double Target10Rate { get; set; }
+        public double Target11Rate { get; set; }
+    }
+}
--- DiceRoller/Program.cs
+++ DiceRoller/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CsvHelper.Excel;
+using DiceRoller.Csv;
 using DiceRoller.Roller;
+using DocumentFormat.OpenXml.Office2013.Word;
 
 Console.WriteLine("Hello, World!");
 
@@ -18,7 +21,7 @@ var resultDict = new Dictionary<string, Dictionary<int,double>>();
 foreach (var roller in rollers)
 {
     resultDict[roller.GetType().Name] = new Dictionary<int, double>();
-    foreach (var target in Enumerable.Range(2, 11))
+    foreach (var target in Enumerable.Range(2, 10)) // max is 11
     {
 
         int i = iterations;
@@ -37,4 +40,28 @@ foreach (var roller in rollers)
 
         
     }
+}
+
+
+var csvRows = new List<RowDto>();
+
+foreach (var resultDictKey in resultDict.Keys)
+{
+    var csvRow = new RowDto() {RollerName = resultDictKey};
+    csvRow.Target2Rate = resultDict[resultDictKey][2];
+    csvRow.Target3Rate = resultDict[resultDictKey][3];
+    csvRow.Target4Rate = resultDict[resultDictKey][4];
+    csvRow.Target5Rate = resultDict[resultDictKey][5];
+    csvRow.Target6Rate = resultDict[resultDictKey][6];
+    csvRow.Target7Rate = resultDict[resultDictKey][7];
+    csvRow.Target8Rate = resultDict[resultDictKey][8];
+    csvRow.Target9Rate = resultDict[resultDictKey][9];
+    csvRow.Target10Rate = resultDict[resultDictKey][10];
+    csvRow.Target11Rate = resultDict[resultDictKey][11];
+    csvRows.Add(csvRow);
+}
+
+using (var writer = new ExcelWriter(@"c:\temp\result.xlsx"))
+{
+    writer.WriteRecords(csvRows);
 }
