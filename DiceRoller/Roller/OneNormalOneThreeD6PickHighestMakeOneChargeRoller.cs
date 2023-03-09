6d15ddd3151d995f738c3ba5fb84436dbd223f67
Per Grönberg <A2010438@nbsemp.no>
2023-03-09 11:06:18 +0100

    Added rollers

---
+++ DiceRoller/Roller/OneNormalOneThreeD6PickHighestMakeOneChargeRoller.cs
@@ -0,0 +1,29 @@
+﻿namespace DiceRoller.Roller;
+
+class OneNormalOneThreeD6PickHighestMakeOneChargeRoller : NormalChargeRoller
+{
+
+    public override string Description => "One normal and one 3d6-discard lowest dice charge, make at least one";
+
+
+    protected override bool RollDices(int target)
+    {
+        var a = GetDiceRoll();
+        var b = GetDiceRoll();
+        var c = GetDiceRoll();
+
+        var r = a + b + c;
+
+        var min = Math.Min(Math.Min(a, b), c);
+        return (r-min) >= target;
+    }
+
+    public override bool Success(int target)
+    {
+        var _3d6Roll =  RollDices(target) ? 1:0;
+        var normalRoll = base.RollDices(target) ? 1:0;
+
+        return (_3d6Roll + normalRoll) >= 1;
+
+    }
+}
---
+++ DiceRoller/Roller/ThreeNormalChargesMakeOneRoller.cs
@@ -0,0 +1,17 @@
+﻿namespace DiceRoller.Roller;
+
+class ThreeNormalChargesMakeOneRoller : NormalChargeRoller
+{
+
+
+    public override string Description => "Three normal 2d6 charges, make at least one";
+
+    public override bool Success(int target)
+    {
+        var d1 = RollDices(target) ? 1:0;
+        var d2 = RollDices(target) ? 1 : 0;
+        var d3 = RollDices(target) ? 1 : 0;
+        return (d1 + d2 + d3) >= 1;
+    }
+
+}
